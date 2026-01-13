a7c95c26d7ec6c8e9a5937e4dc6832b1beb4711c
unknown <mailasrilakshmi41@gmail.com>
2026-01-13 15:04:28 +0530

    Gmail-to-Sheets-Automation

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Google Sheets Configuration
 SPREADSHEET_ID = '1kQ3eUSmwbmgrfbCRzzlDPcS-HJ7qG2WoEP31DxyBUwQ'
 
-# Optional: You can also define the range here if you renamed your sheet tab
+
 SHEET_NAME = 'Inbox_data'
 RANGE_NAME = f'{SHEET_NAME}!A1'
 
